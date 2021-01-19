1293689789bff411fa7f2bcf4cb40a2befd02b2d
Brijkishor24 <71825478+Brijkishor24@users.noreply.github.com>
2021-01-19 09:37:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function navigate(){
+    window.location="Mobile.HTML";
+}
+function navigate2(){
+    window.location="Laptop.HTML";
+}
+function navigate3(){
+    window.location="Chair.HTML";
+}
+function navigate4(){
+    window.location="TV.HTML";
+}
+function navigate5(){
+    window.location="AC.HTML";
+}
